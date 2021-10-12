7cc082de8fd54efed6a91a2cc1d037f3b1625a11
vogtalex <vogtalex@oregonstate.edu>
2021-10-11 20:11:31 -0700

    Attempted to apply FGSD to MNIST digit data set

---
+++ MNIST_Digit_FGSD_ALT.py
@@ -0,0 +1,170 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+training_data = torchvision.datasets.MNIST(
+    '/files/',
+    train=True,
+    download=True,
+    transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                              torchvision.transforms.Normalize((0.1307,), (0.3081,))]
+                                             )
+)
+
+
+test_data = torchvision.datasets.MNIST(
+    '/files/',
+    train=False,
+    download=True,
+    transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                              torchvision.transforms.Normalize((0.1307,), (0.3081,))]
+                                             )
+)
+
+train_dataloader = DataLoader(training_data, 64, shuffle=True)
+test_dataloader = DataLoader(test_data, 1000, shuffle=True)
+
+epochs = 3
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+batch_size = 64
+
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork()
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+use_cuda = True
+print("CUDA Available: ", torch.cuda.is_available())
+device = torch.device("cuda" if (
+    use_cuda and torch.cuda.is_available()) else "cpu")
+
+epsilon = 0.05
+
+
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (data, target) in enumerate(train_dataloader):
+        # Compute prediction and loss
+        pred = model(data)
+        loss = loss_fn(pred, target)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(data)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+def adv_test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for data, y in dataloader:
+            pred = model(data)
+            optimizer.zero_grad()
+            loss = loss_fn(pred, y)
+            loss.backward(retain_graph=True)
+            sign_data_grad = loss.sign()
+            perturbed_image = data + epsilon*sign_data_grad
+            perturbed_image = torch.clamp(perturbed_image, 0, 1)
+            pred = model(perturbed_data)
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    loss /= num_batches
+    correct /= size
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+epochs = 0
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
+
+"""
+def fgsm_attack(image, epsilon, y):
+    # Collect the element-wise sign of the data gradient
+    pred = model(image)
+    loss = loss_fn(pred, y)
+    loss.backward(retain_graph=True)
+    sign_data_grad = loss.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+
+for data, y in test_dataloader:
+    perturbed_data = fgsm_attack(data, epsilon, y)
+"""
+adv_test_loop(test_dataloader, model, loss_fn)
